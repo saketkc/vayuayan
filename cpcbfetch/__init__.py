3a332590c54d2720f49998bcc6cd582dcaa31209
Mahesh Kumar <mahesh.99prajapat@gmail.com>
2025-09-10 10:46:51 +0530

    [Chore] file name changed from api_client.py to air_quality_client.py for better convention

--- cpcbfetch/__init__.py
+++ cpcbfetch/__init__.py
@@ -7,7 +7,7 @@ __author__ = "Saket Choudhary"
 __email__ = "saketc@iitb.ac.in"
 
 from .client import CPCBClient
-from .aqi_client import AQIClient
+from .air_quality_client import AQIClient
 
 __all__ = [
     "CPCBClient",
